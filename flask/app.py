d01cf0507d83c6b6af76056746c5d23b05618539
Shikher Kumar <75657175+SHIKHER09@users.noreply.github.com>
2023-08-26 21:02:34 +0530

    Revert "fixed the code"

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,8 @@
+# app.py
+
 from flask import Flask, render_template, request
 import pickle
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
-import train_and_save_model as Tmodel
-
 
 app = Flask(__name__)
 
@@ -17,22 +16,31 @@ with open('vectorizer.pkl', 'rb') as vectorizer_file:
 # Load your data (replace with your actual data loading code)
 data = pd.read_csv('Mental_Health_FAQ.csv', delimiter=',')
 
-# Preprocess the questions for similarity matching
-data['Processed_Questions'] = data['Questions'].apply(lambda x: x.lower())
-
 # Define the home page route
+
+
 @app.route('/')
 def home():
     return render_template('home.html')  # Render the home page template
 
 # Define the route to handle user inputs
+
+
 @app.route('/chat', methods=['POST'])
 def chat():
+
     user_input = request.form['user_input']
-    
-    response = Tmodel.user_input(user_input)
+
+    user_input_vec = vectorizer.transform([user_input.lower()])
+    predicted_intent = model.predict(user_input_vec)[0]
+
+    response = data[data['Questions'] == predicted_intent]['Answers'].values[0] if predicted_intent in data[
+        'Questions'].values else "I'm sorry, I don't have a response for that question."
 
     return render_template('chat.html', user_input=user_input, response=response)
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+app.run(host='0.0.0.0', port=81)
